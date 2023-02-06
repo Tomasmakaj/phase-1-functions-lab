078d16daec0b4d0af12fee49d035fbb0f32cd28c
Tomas Makaj <Tmakajcoding@gmail.com>
2023-02-06 13:33:40 -0500

    commit tree 145cb0fbb267fbdd3b0cd90770a5e3f8cd07c058

--- index.js
+++ index.js
@@ -1,48 +1,35 @@
-// Code your solution in this file!
-function distanceFromHqInBlocks (someValue) {
-    //returns the number of blocks given a value
-    if (someValue<42){
-      return(42-someValue);
+function distanceFromHqInBlocks(blockNumber) {
+    if (blockNumber > 42) {
+        return blockNumber - 42;
+    } else {
+        return 42 - blockNumber;
     }
-    else {
-  
-    return(someValue-42);
-  }
-  }
+}
 
-// function distanceFromHqInBlocks(someValue) {
-//     return someValue = 1;
-//     if (someValue<42){
-//         return (42-someValue);
-//     }
-//     else {
-//     return(someValue-42);
-//     }
-// }
-function distanceFromHqInFeet(someValue){
-    if (someValue<42){
-        return(42-someValue)*264
-      }
-      else {
-    
-      return(someValue-42)*264
+
+function distanceFromHqInFeet(blockNumber) {
+    return distanceFromHqInBlocks(blockNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264
     }
 }
-function distanceTravelledInFeet(start,end){
-    return Math.abs(start-end)*264
-}
-function calculatesFarePrice(start, destination){
-    let distance = distanceTravelledInFeet(start,destination);
-    if (distance<=400){
+
+function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+
+    if (distance <= 400) {
         return 0;
-    }
-    if (distance>=400 && distance<=2000){
-        return (distance-400) * 0.02;
-    }
-    else if (distance>= 2000 && distance <=2500){
+    } else if (distance > 400 && distance <= 2000) {
+        return .02 * (distance - 400);
+    } else if (distance > 2000 && distance < 2500) {
         return 25;
-    }
-    else if(distance>2500){
-        return "cannot travel that far";
+    } else {
+        return 'cannot travel that far';
     }
 }
+
